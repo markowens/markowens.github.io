993107a37e9851593c0e1d517f105ab69c06c621
Mark Owens <jmark99@apache.org>
2021-05-26 15:20:37 -0400

    added udemy puzzles

---
+++ training/udem/puzzles/src/practice/strings/StringsPratice.java
@@ -0,0 +1,11 @@
+package practice.strings;
+
+public class StringsPratice {
+
+	public static void main(String [] args) {
+		System.out.println();
+		String a = "hello";
+		System.out.println(a.substring(2, 5));
+	}
+	
+}
